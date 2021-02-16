3047300f7a849ff08c237f727686256cc348a80f
paulhogan <paul.tanbunan@gmail.com>
2021-02-16 19:22:55 +0700

    f

--- c14180116/index.php
+++ c14180116/index.php
@@ -4,19 +4,21 @@ c14180116-paul
 <body>
   <br>
   <input type="date" id="date">
+  <input type="date" id="day"><br>
   <input type="submit" onclick="count()"><br>
 </body>
 <script type= "text/javascript">
     function count() {
-      var tgl = document.getElementById("date").value;
-      dif = new Date(tgl.toString());
-      var today = new Date("2021/02/16");
-      if(dif.getTime() > today.getTime()){
-        var diftime = dif.getTime() - today.getTime();
+      var tgl = document.getElementById("birthday").value;
+      date = new Date(tgl.toString());
+      var toda = document.getElementById("dayday").value;
+      today = new Date(toda.toString());
+      if(date.getTime() > today.getTime()){
+        var diftime = date.getTime() - today.getTime();
         var difday = diftime / (1000360024);
       }
       else{
-        var diftime =  today.getTime() - dif.getTime();
+        var diftime =  today.getTime() - date.getTime();
         var difday = diftime / (1000360024);
       }
       document.getElementById("demo").innerHTML = difday + " Day";
